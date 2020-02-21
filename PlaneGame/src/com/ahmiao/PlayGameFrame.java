29e0df2221709e3411bd06303c1ada5244f210fa
Ahmiao <1426257066@qq.com>
2020-02-21 23:28:05 +0800

    新增

--- PlaneGame/src/com/ahmiao/PlayGameFrame.java
+++ PlaneGame/src/com/ahmiao/PlayGameFrame.java
@@ -1,6 +1,8 @@
 package com.ahmiao;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * 飞机大战的主窗口
@@ -11,7 +13,17 @@ public class PlayGameFrame extends JFrame {
      * 初始化窗口
      */
     public void launchFrame(){
+        //设窗口的一些属性
         this.setTitle("Ahmiao'Game");
         this.setVisible(true);
+        this.setSize(500,500);
+        this.setLocation(300,300);
+        //增加关闭事件
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
