bdc2d20e9469d01a772b6993641eb2dfccde7a89
Ahmiao <1426257066@qq.com>
2020-02-23 23:23:41 +0800

    Spring Boot 整合视图层技术

---
+++ springmergeview/src/main/java/com/ahmiao/springmergeview/SpringmergeviewApplication.java
@@ -0,0 +1,13 @@
+package com.ahmiao.springmergeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmergeviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmergeviewApplication.class, args);
+    }
+
+}
---
+++ springmergeview/src/test/java/com/ahmiao/springmergeview/SpringmergeviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmiao.springmergeview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmergeviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
