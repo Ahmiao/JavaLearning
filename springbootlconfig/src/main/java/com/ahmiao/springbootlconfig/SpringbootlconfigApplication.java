8c4c177515e401366c346699fc4e0ed79be53f06
Ahmiao <1426257066@qq.com>
2020-02-23 22:02:55 +0800

    Spring Boot基础配置的学习

---
+++ springbootlconfig/src/main/java/com/ahmiao/springbootlconfig/SpringbootlconfigApplication.java
@@ -0,0 +1,13 @@
+package com.ahmiao.springbootlconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootlconfigApplication.class, args);
+    }
+
+}
---
+++ springbootlconfig/src/test/java/com/ahmiao/springbootlconfig/SpringbootlconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmiao.springbootlconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
