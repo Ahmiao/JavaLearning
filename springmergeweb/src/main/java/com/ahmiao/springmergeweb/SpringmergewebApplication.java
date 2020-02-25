c060c129062bf6012b4f4e9a5a97045e5dbbb6b2
Ahmiao <1426257066@qq.com>
2020-02-25 20:08:56 +0800

    Spring Boot整合Web开发

---
+++ springmergeweb/src/main/java/com/ahmiao/springmergeweb/SpringmergewebApplication.java
@@ -0,0 +1,13 @@
+package com.ahmiao.springmergeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmergewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmergewebApplication.class, args);
+    }
+
+}
---
+++ springmergeweb/src/test/java/com/ahmiao/springmergeweb/SpringmergewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmiao.springmergeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmergewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
